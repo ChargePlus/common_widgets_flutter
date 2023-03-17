41c0795faa63ad737eb0f74c0e67746b3015e236
Ezra <ezra.gunn@chargeplus.com>
2023-03-17 18:11:21 +0800

    refactor(ticker): use difference instead

--- lib/src/ticker.dart
+++ lib/src/ticker.dart
@@ -1,5 +1,3 @@
-import 'package:jiffy/jiffy.dart';
-
 /// {@template ticker}
 /// A ticker that displays ticks every second
 /// {@endtemplate}
@@ -19,30 +17,7 @@ class Ticker {
   Stream<Duration> elapsedFrom(DateTime startedAt) {
     return Stream.periodic(const Duration(seconds: 1), (_) {
       final now = DateTime.now();
-
-      return Duration(
-        milliseconds: int.parse(
-          Jiffy([
-            now.year,
-            now.month,
-            now.day,
-            now.hour,
-            now.minute,
-            now.second
-          ]).diff(
-            [
-              startedAt.year,
-              startedAt.month,
-              startedAt.day,
-              startedAt.hour,
-              startedAt.minute,
-              startedAt.second
-            ],
-            Units.MILLISECOND,
-            true,
-          ).toString(),
-        ),
-      );
+      return now.difference(startedAt);
     });
   }
 }
