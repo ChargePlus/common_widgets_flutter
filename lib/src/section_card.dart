7faa5e59dc2967725bd41791d8c7528529870dda
Ezra <ezra.gunn@chargeplus.com>
2023-06-19 18:24:43 +0800

    style(section_card): add opacity to filled card

--- lib/src/section_card.dart
+++ lib/src/section_card.dart
@@ -79,7 +79,8 @@ class SectionCard extends StatelessWidget {
         );
       case CardType.filled:
         return Card(
-          color: color ?? Theme.of(context).colorScheme.surfaceVariant,
+          color: color ??
+              Theme.of(context).colorScheme.surfaceVariant.withOpacity(0.7),
           elevation: 0,
           margin: margin,
           child: Padding(
