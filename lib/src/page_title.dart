2dde127bc2ab7a9fd8a13cac2211f054a29419d0
luqmantalib92 <luqmantalib92@gmail.com>
2023-07-06 13:11:41 +0800

    refactor(page_title): remove MediaQuery

--- lib/src/page_title.dart
+++ lib/src/page_title.dart
@@ -19,17 +19,14 @@ class PageTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery(
-      data: const MediaQueryData(),
-      child: ListTile(
-        contentPadding: EdgeInsets.zero,
-        title: AnimatedDefaultTextStyle(
-          style: Theme.of(context).textTheme.titleLarge!,
-          duration: const Duration(milliseconds: 200),
-          child: title,
-        ),
-        subtitle: subtitle,
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      title: AnimatedDefaultTextStyle(
+        style: Theme.of(context).textTheme.titleLarge!,
+        duration: const Duration(milliseconds: 200),
+        child: title,
       ),
+      subtitle: subtitle,
     );
   }
 }
