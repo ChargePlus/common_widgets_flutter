921561c0ba75f2fa6856726dc8316b0eded8b333
Ezra <ezra.gunn@chargeplus.com>
2024-07-01 17:33:22 +0800

    refactor(annotations): add asserts, simplify widgets

--- lib/src/annotations.dart
+++ lib/src/annotations.dart
@@ -15,7 +15,22 @@ class Annotations extends StatelessWidget {
     this.subscriptionAnnotationTranslation,
     this.serviceFeeAnnotationTranslation,
     this.discountQuotaAnnotationTranslation,
-  });
+  })  : assert(
+          hasSubscription ^ (subscriptionAnnotationTranslation != null),
+          'subscriptionAnnotationTranslation is needed',
+        ),
+        assert(
+          hasTax ^ (taxAnnotationTranslation != null),
+          'taxAnnotationTranslation is needed',
+        ),
+        assert(
+          hasServiceFee ^ (serviceFeeAnnotationTranslation != null),
+          'serviceFeeAnnotationTranslation is needed',
+        ),
+        assert(
+          hasDiscountQuota ^ (discountQuotaAnnotationTranslation != null),
+          'discountQuotaAnnotationTranslation is needed',
+        );
 
   /// This property is used to show subscription annotation
   final bool hasSubscription;
@@ -32,146 +47,41 @@ class Annotations extends StatelessWidget {
   /// This property is used to show translated tax label
   ///
   /// Eg. Before 8% GST
-  final String? taxAnnotationTranslation;
+  final Text? taxAnnotationTranslation;
 
   /// This property is used to show translated subscription label
   ///
   /// Eg. Above charging fees apply after monthly subscription quota is exceeded
-  final String? subscriptionAnnotationTranslation;
+  final Text? subscriptionAnnotationTranslation;
 
   /// This property is used to show translated service fee label
   ///
   /// Eg.
-  final String? serviceFeeAnnotationTranslation;
+  final Text? serviceFeeAnnotationTranslation;
 
   /// This property is used to show translated discounted quota label
-  final String? discountQuotaAnnotationTranslation;
+  final Text? discountQuotaAnnotationTranslation;
 
   @override
   Widget build(BuildContext context) {
-    if (hasTax || hasSubscription || hasServiceFee) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _TaxType(
-            taxAnnotationTranslation: taxAnnotationTranslation,
-            hasTax: hasTax,
-          ),
-          _ServiceFeeAnnotation(
-            hasServiceFee: hasServiceFee,
-            serviceFeeAnnotationTranslation: serviceFeeAnnotationTranslation,
-          ),
-          _SubscriptionAnnotation(
-            hasSubscription: hasSubscription,
-            subscriptionAnnotationTranslation:
-                subscriptionAnnotationTranslation,
-          ),
-          _DiscountQuotaAnnotation(
-            hasDiscountQuota: hasDiscountQuota,
-            discountQuotaAnnotationTranslation:
-                discountQuotaAnnotationTranslation,
-          ),
-        ],
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-}
-
-class _TaxType extends StatelessWidget {
-  const _TaxType({
-    required this.taxAnnotationTranslation,
-    this.hasTax = false,
-  });
-
-  final bool hasTax;
-  final String? taxAnnotationTranslation;
-
-  @override
-  Widget build(BuildContext context) {
-    if (hasTax) {
-      return Text(
-        taxAnnotationTranslation ?? '',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+    if (hasTax || hasSubscription || hasServiceFee || hasDiscountQuota) {
+      return DefaultTextStyle(
+        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
               color: Theme.of(context).textTheme.bodySmall?.color,
             ),
         textAlign: TextAlign.end,
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-}
-
-class _SubscriptionAnnotation extends StatelessWidget {
-  const _SubscriptionAnnotation({
-    required this.subscriptionAnnotationTranslation,
-    this.hasSubscription = false,
-  });
-
-  final bool hasSubscription;
-  final String? subscriptionAnnotationTranslation;
-
-  @override
-  Widget build(BuildContext context) {
-    if (hasSubscription) {
-      return Text(
-        subscriptionAnnotationTranslation ?? '',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).textTheme.bodySmall?.color,
-            ),
-        textAlign: TextAlign.end,
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-}
-
-class _ServiceFeeAnnotation extends StatelessWidget {
-  const _ServiceFeeAnnotation({
-    required this.serviceFeeAnnotationTranslation,
-    this.hasServiceFee = false,
-  });
-
-  final bool hasServiceFee;
-  final String? serviceFeeAnnotationTranslation;
-
-  @override
-  Widget build(BuildContext context) {
-    if (hasServiceFee) {
-      return Text(
-        serviceFeeAnnotationTranslation ?? '',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).textTheme.bodySmall?.color,
-            ),
-        textAlign: TextAlign.end,
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-}
-
-class _DiscountQuotaAnnotation extends StatelessWidget {
-  const _DiscountQuotaAnnotation({
-    required this.discountQuotaAnnotationTranslation,
-    this.hasDiscountQuota = false,
-  });
-
-  final bool hasDiscountQuota;
-  final String? discountQuotaAnnotationTranslation;
-
-  @override
-  Widget build(BuildContext context) {
-    if (hasDiscountQuota) {
-      return Text(
-        discountQuotaAnnotationTranslation ?? '',
-        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: Theme.of(context).textTheme.bodySmall?.color,
-            ),
-        textAlign: TextAlign.end,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            if (hasTax) taxAnnotationTranslation ?? const SizedBox.shrink(),
+            if (hasServiceFee)
+              serviceFeeAnnotationTranslation ?? const SizedBox.shrink(),
+            if (hasSubscription)
+              subscriptionAnnotationTranslation ?? const SizedBox.shrink(),
+            if (hasDiscountQuota)
+              discountQuotaAnnotationTranslation ?? const SizedBox.shrink(),
+          ],
+        ),
       );
     } else {
       return const SizedBox.shrink();
