4ea4eaafdf8c3fdc51838e11aadc36c1a2f35a57
luqmantalib92 <luqmantalib92@gmail.com>
2025-01-07 14:39:00 +0800

    feat(section_title): remove list tile and use only text widget

--- lib/src/section_title.dart
+++ lib/src/section_title.dart
@@ -8,33 +8,24 @@ class SectionTitle extends StatelessWidget {
   const SectionTitle({
     required this.title,
     super.key,
-    this.subtitle,
     this.color,
   });
 
   /// This property is used for a section's title
   final String title;
 
-  /// This property is used for a section's subtitle
-  final String? subtitle;
-
   /// This property is used for the font color
   final Color? color;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      dense: subtitle == null,
-      title: AnimatedDefaultTextStyle(
-        style: Theme.of(context).textTheme.titleMedium!.copyWith(
-              color: color ?? Theme.of(context).colorScheme.primary,
-              fontWeight: FontWeight.w600,
-            ),
-        duration: const Duration(milliseconds: 200),
-        child: Text(title),
-      ),
-      subtitle: subtitle != null ? Text(subtitle!) : null,
+    return AnimatedDefaultTextStyle(
+      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+            color: color ?? Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.w600,
+          ),
+      duration: const Duration(milliseconds: 200),
+      child: Text(title),
     );
   }
 }
